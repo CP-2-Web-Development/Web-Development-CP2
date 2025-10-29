eab34269ae56a4610615fc7e37ca71a8e1d0bdf2
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2025-10-29 19:47:35 -0300

    10 - feact: High Contrast

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,15 @@
 //FUNÇÃO TROCAR
 function trocar(modo) {
+  // Remove todas as classes antes de adicionar a nova
+  document.body.classList.remove('dark-mode', 'high-contrast-mode');
+
   if (modo === 'escuro') {
     document.body.classList.add('dark-mode');
     localStorage.setItem('modo', 'escuro');
+  } else if (modo === 'contraste') {
+    document.body.classList.add('high-contrast-mode');
+    localStorage.setItem('modo', 'contraste');
   } else {
-    document.body.classList.remove('dark-mode');
     localStorage.setItem('modo', 'claro');
   }
 }
@@ -13,6 +18,8 @@ window.onload = function () {
   const temaSalvo = localStorage.getItem('modo');
   if (temaSalvo === 'escuro') {
     document.body.classList.add('dark-mode');
+  } else if (temaSalvo === 'contraste') {
+    document.body.classList.add('high-contrast-mode');
   }
 };
 
